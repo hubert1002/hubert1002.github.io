a3121e2a63da76539c56281bc01467ca5127fcfb
hubert1002 <hubert1002@sina.com>
2016-04-22 20:32:32 +0800

    Update base.js

--- public/js/base.js
+++ public/js/base.js
@@ -97,13 +97,15 @@ $(document).ready(function() {
         showTab(false);
     }
     
-      //
-  var ds = document.createElement('script');
+var duoshuoQuery = {short_name:"hubert1002"};
+	(function() {
+		var ds = document.createElement('script');
 		ds.type = 'text/javascript';ds.async = true;
 		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
 		ds.charset = 'UTF-8';
 		(document.getElementsByTagName('head')[0] 
 		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
     
   });
 
